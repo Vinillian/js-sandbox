985e8934b4740b848b47dc60b81c8710fd285231
Vinillian <ber07@inbox.ru>
2026-05-30 20:40:20 +0300

    feat(calculator): add multiplication operation
    - Add multiply button (id='multBtn') and corresponding case in calculate()
    - Extend switch statement with 'mult' case using *
    - Update button click handler for multiplication

--- 01_simple_basic_calc/script.js
+++ 01_simple_basic_calc/script.js
@@ -2,6 +2,7 @@ const input1 = document.getElementById('num1');
 const input2 = document.getElementById('num2');
 const buttonSum = document.getElementById('sumBtn');
 const buttonMin = document.getElementById('minBtn');
+const buttonMult = document.getElementById('multBtn');
 const resultSpan = document.getElementById('result');
 
 
@@ -12,6 +13,7 @@ function calculate(operation) {
     switch(operation) {
         case 'sum': result = val1 + val2; break;
         case 'min': result = val1 - val2; break;
+        case 'mult': result = val1 * val2; break;
         default: result = 'error';
     }
 
@@ -20,3 +22,4 @@ function calculate(operation) {
 
 buttonSum.onclick = () => calculate('sum');
 buttonMin.onclick = () => calculate('min');
+buttonMult.onclick = () => calculate('mult');
